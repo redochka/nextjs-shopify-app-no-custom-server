ec1d1bb8fc3767634febf5db4876c8c460523f6c
reda <reda.abdi@gmail.com>
2022-02-10 10:44:05 +0100

    remove unused import

--- components/SessionProvider.js
+++ components/SessionProvider.js
@@ -1,20 +1,18 @@
-
-import { useEffect } from 'react';
-import {useRouter} from "next/router"
-import { getSessionToken } from "@shopify/app-bridge-utils";
-import { useAppBridge } from "@shopify/app-bridge-react";
+import {useEffect} from 'react';
+import {getSessionToken} from "@shopify/app-bridge-utils";
+import {useAppBridge} from "@shopify/app-bridge-react";
 
 export default function SessionProvider({children}) {
   const app = useAppBridge();
-  
+
   useEffect(async () => {
     const session = await getSessionToken(app);
-    
+
     if (!session) {
       window.location.pathname = `/api/auth/shopify/login`;
     }
   }, []);
-  
+
   return <>{children}</>;
 }
 
